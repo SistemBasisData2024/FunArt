8faec62255ca0c25e1aeb0b48b4152772eadfad5
Andrew Kristofer Jian <andrew.kristofer@ui.ac.id>
2024-06-03 05:20:35 +0700

    Creating BackEnd

--- BE/repositories/repository.account.js
+++ BE/repositories/repository.account.js
@@ -1,25 +1,49 @@
-const { Pool } = require('pg');
+// const { Pool } = require('pg');
 
-const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_ACCOUNT,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
-});
+// const pool = new Pool({
+//     user: process.env.DB_USER,
+//     host: process.env.DB_HOST,
+//     database: process.env.DB_DATABASE,
+//     password: process.env.DB_PASSWORD,
+//     port: process.env.DB_PORT,
+// });
 
-// const getAccounts = async (npm, password) => {
-//     const { rows } = await pool.query('SELECT * FROM account WHERE npm = $1 AND password = $2', [npm, password]);
-//     return rows;
-// };
+// async function loginAcc(username, password) {
+//   try {
+//     const query = {
+//       text: 'SELECT acc_id, points, contents_id FROM account WHERE username = $1 AND password = $2',
+//       values: [username, password],
+//     };
 
-// const createAccount = async (npm, password, display_name) => {
-//     await pool.query('INSERT INTO account (npm, password, display_name) VALUES ($1, $2, $3)', [npm, password, display_name]);
-// };
+//     const result = await pool.query(query);
 
-// // const getAccountById = async ()
+//     if (result.rows.length === 1) {
+//       return result.rows[0];
+//     } else {
+//       return null; // No account found with the provided username and password
+//     }
+//   } catch (error) {
+//     console.error('Error executing query', error);
+//     throw error;
+//   }
+// }
+
+// async function registerAcc(username, password) {
+//   try {
+//     const query = {
+//       text: 'INSERT INTO account (acc_id, acc_type, username, password, points, contents_id) VALUES ($1, $2, $3, $4, $5, $6) RETURNING acc_id',
+//       values: [generateRandomAlphanumeric(10), 'registered', username, password, 0, []],
+//     };
+
+//     const result = await pool.query(query);
+//     return result.rows[0].acc_id;
+//   } catch (error) {
+//     console.error('Error executing query', error);
+//     throw error;
+//   }
+// }
 
 // module.exports = {
-//     getAccount,
-//     createAccount
+//   loginUser,
+//   registerUser,
 // };
--- BE/repositories/repository.content.js
+++ BE/repositories/repository.content.js
@@ -1,10 +1,30 @@
-const { Pool } = require('pg');
+// const { Pool } = require('pg');
 
-const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_CONTENT,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
-});
+// const pool = new Pool({
+//     user: process.env.DB_USER,
+//     host: process.env.DB_HOST,
+//     database: process.env.DB_DATABASE,
+//     password: process.env.DB_PASSWORD,
+//     port: process.env.DB_PORT,
+// });
 
+// async function postContent(acc_id, content_type, content_desc, tag, content_title) {
+//   try {
+//     const content_id = generateRandomAlphanumeric(10);
+
+//     const query = {
+//       text: 'INSERT INTO content (content_id, acc_id, content_type, content_desc, tag, likes, comments_id, content_title) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING content_id',
+//       values: [content_id, acc_id, content_type, content_desc, tag, 0, null, content_title],
+//     };
+
+//     await pool.query(query);
+//     return content_id;
+//   } catch (error) {
+//     console.error('Error executing query', error);
+//     throw error;
+//   }
+// }
+
+// module.exports = {
+//   postContent,
+// };
